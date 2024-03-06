f5272465c3acded714242d34013678e779a9652f
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-06 15:58:51 +0100

    Moved transform.

--- classification.py
+++ classification.py
@@ -8,6 +8,12 @@ LABELS_URL = 'https://storage.googleapis.com/bit_models/ilsvrc2012_wordnet_lemma
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
 def download_labels():
     filename = 'labels.txt'
 
@@ -23,12 +29,6 @@ def generate(image):
 
     model = torchvision.models.resnet50(pretrained=True).to(device)
     model.eval()
-
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
 
     logits = model(transform(image).unsqueeze(0)).to(device)
     probs = torch.nn.Softmax(dim=-1)(logits)
