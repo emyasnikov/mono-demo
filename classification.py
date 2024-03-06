2174e6ba65981edae112fee4f64d84ce4e864669
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-06 15:34:41 +0100

    Added labels download.

--- classification.py
+++ classification.py
@@ -1,2 +1,15 @@
+import os.path
+import urllib.request
+
+LABELS_URL = 'https://storage.googleapis.com/bit_models/ilsvrc2012_wordnet_lemmas.txt'
+
+def download_labels():
+    filename = 'labels.txt'
+
+    if not os.path.exists(filename):
+        urllib.request.urlretrieve(LABELS_URL, filename)
+
+    return filename
+
 def generate(image):
-    pass
+    labels = download_labels()
