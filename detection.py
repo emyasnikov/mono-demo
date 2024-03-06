e1b44721074214d71dbc62710bd97dd3e575c0ab
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-06 16:08:04 +0100

    Added detection output.

--- detection.py
+++ detection.py
@@ -4,3 +4,15 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def generate(image):
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to(device)
+
+    objects = model(image).pandas().xyxy[0].to_dict(orient='records')
+
+    output = {}
+
+    for obj in objects:
+        if obj['name'] not in output:
+            output[obj['name']] = 1
+        else:
+            output[obj['name']] += 1
+
+    return output
