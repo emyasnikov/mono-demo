35ab3be2b064008f31f2536735880250ef8743fd
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-06 15:37:52 +0100

    Added classification model.

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
 import os.path
+import torchvision
 import urllib.request
 
 LABELS_URL = 'https://storage.googleapis.com/bit_models/ilsvrc2012_wordnet_lemmas.txt'
@@ -15,3 +16,6 @@ def generate(image):
     labels = download_labels()
 
     label_map = dict(enumerate(open(labels)))
+
+    model = torchvision.models.resnet50(pretrained=True)
+    model.eval()
