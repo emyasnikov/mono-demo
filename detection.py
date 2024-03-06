9f182d872abce3d8574d7f37219d064784b11b0a
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-06 16:06:51 +0100

    Added device.

--- detection.py
+++ detection.py
@@ -1,4 +1,6 @@
 import torch
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 def generate(image):
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to(device)
