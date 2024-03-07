7726b27a9bb7d988fa684a30638ac0f8148a4f33
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-07 14:12:21 +0100

    Added deepl server url.

--- translation.py
+++ translation.py
@@ -6,7 +6,7 @@ target_lang = config.get('DEEPL_TARGET_LANG')
 
 def translate(input):
     if config.get('DEEPL') == 'true':
-        translator = deepl.Translator(auth_key)
+        translator = deepl.Translator(auth_key, server_url=config.get('DEEPL_SERVER_URL'))
 
         return translator.translate_text(input, target_lang=target_lang)
 
