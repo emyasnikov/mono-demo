9614e4b41dd90ae2fc45bea04c519ee3ba702534
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-07 10:34:04 +0100

    Added deepl activation config.

--- translation.py
+++ translation.py
@@ -3,7 +3,11 @@ import deepl
 
 auth_key = config.get('DEEPL_AUTH_KEY')
 target_lang = config.get('DEEPL_TARGET_LANG')
-translator = deepl.Translator(auth_key)
 
 def translate(input):
-    return translator.translate_text(input, target_lang=target_lang)
+    if config.get('DEEPL') == 'true':
+        translator = deepl.Translator(auth_key)
+
+        return translator.translate_text(input, target_lang=target_lang)
+
+    return input
