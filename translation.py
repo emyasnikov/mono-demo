eff3d76a5389f2cb4dfc1d39b13a8a8aeaf493b2
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-07 14:40:06 +0100

    Added fallback translation.

--- translation.py
+++ translation.py
@@ -1,13 +1,16 @@
 import config
 import deepl
+import transformers
 
 auth_key = config.get('DEEPL_AUTH_KEY')
 target_lang = config.get('DEEPL_TARGET_LANG')
 
-def translate(input):
+def translate(input, max_length=512):
     if config.get('DEEPL') == 'true':
         translator = deepl.Translator(auth_key, server_url=config.get('DEEPL_SERVER_URL'))
 
         return translator.translate_text(input, target_lang=target_lang)
 
-    return input
+    translator = transformers.pipeline('translation_en_to_de', max_length=max_length)
+
+    return translator(input)[0]['translation_text']
