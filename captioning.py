c5923483b65cb172f03deb248fe18f81b9ba06ea
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-07 10:15:53 +0100

    Added captioning model config.

--- captioning.py
+++ captioning.py
@@ -1,16 +1,11 @@
+import config
 import torch
 from transformers import GPT2TokenizerFast, VisionEncoderDecoderModel, ViTImageProcessor
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-models = [
-    'Abdou/vit-swin-base-224-gpt2-image-captioning',
-    'nlpconnect/vit-gpt2-image-captioning',
-    'ydshieh/vit-gpt2-coco-en',
-]
-
 def generate(image, greedy=True, model_name=None):
-    model = models[model_name] if not model_name == None else models[1]
+    model = model_name if not model_name == None else config.get('MODEL_CAPTIONING')
 
     model_raw = VisionEncoderDecoderModel.from_pretrained(model).to(device)
     tokenizer = GPT2TokenizerFast.from_pretrained(model)
