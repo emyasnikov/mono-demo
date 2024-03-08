411273bf7f377b088761ef7bd01717bb22ef9c74
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-08 14:19:42 +0100

    Added analyze tab.

--- demo.py
+++ demo.py
@@ -5,6 +5,15 @@ import detection
 import gradio as gr
 import json
 
+def predict_analyze(type, input):
+    if type == 'Captioning':
+        return captioning.generate(input)
+    elif type == 'Classification':
+        return classification.generate(input)
+    elif type == 'Detection':
+        return detection.generate(input)
+
+
 def predict_api(input):
     results = {
         'captioning': captioning.generate(input),
@@ -14,18 +23,34 @@ def predict_api(input):
 
     return json.dumps(results, indent=2)
 
+demo_analyze = gr.Interface(
+    allow_flagging=False,
+    css='footer {visibility: hidden}',
+    fn=predict_analyze,
+    inputs=[
+        gr.Dropdown(label='Select', choices=[
+            'Captioning', 'Classification', 'Detection',
+        ]),
+        gr.Image(type='pil', label='Image'),
+    ],
+    outputs=gr.Textbox(label='Output', lines=10),
+    title=config.get('APP_TITLE'),
+)
+
 demo_api = gr.Interface(
     allow_flagging=False,
     css='footer {visibility: hidden}',
     fn=predict_api,
-    inputs=gr.Image(type='pil'),
+    inputs=gr.Image(type='pil', label='Image'),
     outputs=gr.Textbox(label='Preview', lines=10),
     title=config.get('APP_TITLE'),
 )
 
 demo = gr.TabbedInterface([
+    demo_analyze,
     demo_api,
 ], [
+    'Analyze',
     'API',
 ])
 
