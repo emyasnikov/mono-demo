900424e97b358a649fc8fb4f4c363021799e80ea
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-08 13:47:13 +0100

    Added tabs.

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ import detection
 import gradio as gr
 import json
 
-def predict(input):
+def predict_api(input):
     results = {
         'captioning': captioning.generate(input),
         'classification': classification.generate(input),
@@ -14,14 +14,20 @@ def predict(input):
 
     return json.dumps(results, indent=2)
 
-demo = gr.Interface(
+demo_api = gr.Interface(
     allow_flagging=False,
     css='footer {visibility: hidden}',
-    fn=predict,
+    fn=predict_api,
     inputs=gr.Image(type='pil'),
     outputs=gr.Textbox(label='Preview', lines=10),
     title=config.get('APP_TITLE'),
 )
+
+demo = gr.TabbedInterface([
+    demo_api,
+], [
+    'API',
+])
 
 if __name__ == "__main__":
     demo.queue().launch(root_path='/demo')
