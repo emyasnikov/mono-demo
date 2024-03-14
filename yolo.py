a7a2eb88e943c86111282376676e384ad461ef2a
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-14 10:26:15 +0100

    Added object detection.

--- yolo.py
+++ yolo.py
@@ -1,7 +1,16 @@
 import gradio as gr
+from PIL import Image
+from ultralytics import YOLO
 
 def predict(image):
-    return image
+    model = YOLO('yolov8n.pt')
+    results = model.predict(image)
+
+    for r in results:
+        image_array = r.plot()
+        pil_image = Image.fromarray(image_array[..., ::-1])
+
+    return pil_image
 
 demo = gr.Interface(
     fn=predict,
