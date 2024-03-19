9f41c1e4e688ba150572ca310ad48ce003829957
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-19 14:54:29 +0100

    Added root path.

--- chat.py
+++ chat.py
@@ -35,4 +35,4 @@ with gr.Blocks() as demo:
     message.submit(generate, inputs=message, outputs=chatbot)
 
 if __name__ == '__main__':
-    demo.queue().launch()
+    demo.queue().launch(root_path='/chat')
