32e2d1692c7b2c941d9ff9e6d5ee58cbaa5b5035
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-25 16:20:16 +0100

    Changed to gradio demo.

--- sd.py
+++ sd.py
@@ -1,7 +1,20 @@
+import gradio as gr
 import torch
 from diffusers import DiffusionPipeline
 
-pipeline = DiffusionPipeline.from_pretrained('segmind/small-sd', torch_dtype=torch.float32)
-prompt = ''
-image = pipeline(prompt).images[0]
-image.save('image.png')
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+def generate(prompt):
+    pipeline = DiffusionPipeline.from_pretrained('segmind/small-sd', torch_dtype=dtype)
+    image = pipeline(prompt).images[0]
+
+    return image
+
+demo = gr.Interface(
+    fn=generate,
+    inputs=gr.Textbox(lines=8, label="Input Text"),
+    outputs=gr.Image(label="Output Image"),
+)
+
+if __name__ == '__main__':
+    demo.launch()
