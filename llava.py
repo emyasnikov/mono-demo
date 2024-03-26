47600c3d5b8927d4dc76eeb779645f887120163b
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-03-26 10:48:51 +0100

    Changed interface.

--- llava.py
+++ llava.py
@@ -3,7 +3,11 @@ import gradio as gr
 def generate(prompt, history):
     return prompt
 
-demo = gr.ChatInterface(generate)
+demo = gr.Interface(
+    fn=generate,
+    inputs=gr.Textbox(label='Prompt'),
+    outputs=gr.Chatbot(),
+)
 
 if __name__ == '__main__':
     demo.launch()
